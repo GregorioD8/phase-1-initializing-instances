1f7fc29b24f299f70b0f240c8581f33acb1be9c4
Gregorio <Greg.del801@gmail.com>
2023-07-28 15:37:07 -0700

    Classes and Instances code along notes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here
+class Dog {
+    constructor(name,age) {
+        this.name = name
+        this.age = age
+    }
+}
+const redFish = new Fish('Red', 3)
+const blueFish = new Fish('Blue', 1)
+
+
